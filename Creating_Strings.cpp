db8925831660431e33b57699e448c138e4efab5e
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-03-18 20:13:46 +0600

    Creating_Strings.cpp

---
+++ Creating_Strings.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    set<string>st;
+    sort(s.begin(),s.end());
+    st.insert(s);
+    while(next_permutation(s.begin(),s.end())){
+        st.insert(s);
+    }
+    cout<<st.size()<<endl;
+    for(auto it:st){
+        cout<<it<<endl;
+    }
+}
